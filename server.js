fa439916eeaf2de536f5b907b01a30d3dd053240
TheTrepanier <thetrepanier1@Michaels-MacBook-Pro-3.local>
2019-05-21 18:50:01 -0700

    added jawsdb

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 
 app.use(express.static("public"));
 
